aa0ad5b69602fa777bbcc6b78a29d8b80ebb1b4b
Harshavardanan <harshamoorthy22@gmail.com>
2022-11-20 19:24:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Hangman from "./components/Hangman";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Hangman />
+      </header>
+    </div>
+  );
+}
+
+export default App;
